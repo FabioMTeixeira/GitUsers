548ede6d40c1e0cdeb3797ac6efd2afceecfd47a
Fabio <fabiomt.98@hotmail.com>
2023-06-03 17:28:33 -0300

    feat: add button

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,65 +1,60 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Container, Brand } from './styles';
 import axios from 'axios';
 
-async function fetchData(username) {
-  const response = await axios.get(`https://api.github.com/users/${username}`)
-  console.log(response.data);
-  return response.data
-}
-
 async function fetchUserRepos(username) {
   const response = await axios.get(`https://api.github.com/users/${username}/repos`)
-  console.log(response.data);
   return response.data
 }
 
 export function Home() {
   const [user, setUser] = useState(null)
   const [repos, setRepos] = useState([])
+  const [username, setUsername] = useState('')
   
-  useEffect(() => {
-    fetchData('FabioMTeixeira').then(res => setUser(res))
-  },[])
-
-  useEffect(() => {
-    fetchUserRepos('FabioMTeixeira').then(res => setRepos(res))
-  },[])
-
-  if(!user) return 
+  async function handleButtonClick() {
+  await fetchUserRepos(username).then(res => {
+    setRepos(res)
+    setUser(res[0].owner)
+  })
+  }
 
   return (
     <Container>
       <Brand>
         <h1>GitUsers</h1>
-        <input type="text" placeholder='Digite o Username'/>
-        <button>Buscar</button>
+        <input type="text" placeholder='Digite o Username' onChange={(input) => {
+          setUsername(input.target.value)
+        }}/>
+        <button onClick={handleButtonClick}>Buscar</button>
       </Brand>
-        <img src= {user.avatar_url} alt="Foto"/>
-        Nome: {user.name}
-        <a href= {user.html_url} target='_blank' rel="noreferrer">Username: {user.login}</a>
-        <a href={user.repos_url} target='_blank' rel="noreferrer">Quantidade de repositório:{user.public_repos}</a>
-        <table>
-          <thead>
-            <tr>
-              <th>Nome</th>
-              <th>Link do repositório</th>
-              <th>Estrelas</th>
-            </tr>
-          </thead>
-          <tbody>
-
-            {repos.map(repo => {
-              return (
-                <tr key={repo.id}>
-                  <td>{repo.name}</td>
-                  <td><a href={repo.html_url} target='_blank' rel="noreferrer">{repo.html_url}</a></td>
-                  <td>{repo.stargazers_count}</td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
+      {user ? (
+        <div>
+          <img src= {user.avatar_url} alt="Foto"/>
+          <a href= {user.html_url} target='_blank' rel="noreferrer">Username: {user.login}</a>
+          <table>
+            <thead>
+              <tr>
+                <th>Nome</th>
+                <th>Link do repositório</th>
+                <th>Estrelas</th>
+              </tr>
+            </thead>
+            <tbody>
+  
+              {repos.map(repo => {
+                return (
+                  <tr key={repo.id}>
+                    <td>{repo.name}</td>
+                    <td><a href={repo.html_url} target='_blank' rel="noreferrer">{repo.html_url}</a></td>
+                    <td>{repo.stargazers_count}</td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
+        ) : null}
     </Container>
   )
 }
